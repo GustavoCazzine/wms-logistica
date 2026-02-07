accd7620af727cca9a007e1d8f07905b0918b68c
Gustavo Cazzine <cazzinengustavo@gmail.com>
2026-02-07 20:08:20 -0300

    Feat: Implementa modelo abstrato para futuras criaçoes de objetos produtos.

--- src/main/java/br/com/cazzine/model/Produto.java
+++ src/main/java/br/com/cazzine/model/Produto.java
@@ -1,4 +1,41 @@
 package br.com.cazzine.model;
 
-public class Produto {
+abstract class Produto {
+    //Atributos
+    protected String nome;
+    protected double precoCusto;
+
+    //Construtor
+    public Produto(String nome, double precoCusto) {
+        this.nome = nome;
+        this.precoCusto = precoCusto;
+    }
+
+    //Metodos Especiais
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPrecoCusto() {
+        return precoCusto;
+    }
+
+    public void setPrecoCusto(double precoCusto) {
+        this.precoCusto = precoCusto;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Produto: " +
+                "Nome: '" + nome + '\'' +
+                " | Preço de custo: R$=" + precoCusto;
+    }
+
+    //Metodos
+    public void calcularPrecoVenda(){}
 }
